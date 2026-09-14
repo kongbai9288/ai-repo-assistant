5c5ad756a66d4f3ad3eea16d2af375bd6674394b
kongbai9288 <3566500461@qq.com>
2026-09-15 01:43:46 +0800

    fix: SettingsScreen 补 Box / OutlinedButton 导入

--- app/src/main/java/com/kongbai/airepo/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/kongbai/airepo/ui/settings/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package com.kongbai.airepo.ui.settings
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,6 +19,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
