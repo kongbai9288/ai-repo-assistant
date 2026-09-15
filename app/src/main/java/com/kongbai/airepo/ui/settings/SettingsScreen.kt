20471b54d33a89e42171a6774bae16efb4d29cb3
kongbai9288 <3566500461@qq.com>
2026-09-15 14:01:35 +0800

    fix: SettingsScreen 补 androidx.compose.foundation.background 导入

--- app/src/main/java/com/kongbai/airepo/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/kongbai/airepo/ui/settings/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package com.kongbai.airepo.ui.settings
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
